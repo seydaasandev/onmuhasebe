c1d745a7c6885ca3ba1d5d8060801e394ed1c413
Seyda AŞAN <info@seydaasan.com.tr>
2026-03-18 16:01:47 +0300

    rollback dosyaları temizlendi

--- seyda/rollback_multicurrency_20260317/rollback_multicurrency.sql
+++
@@ -1,8 +0,0 @@
--- Multi-currency geri alma SQL
-ALTER TABLE satislar DROP COLUMN IF EXISTS doviz_kuru;
-ALTER TABLE satislar DROP COLUMN IF EXISTS birim_fiyat_doviz;
-ALTER TABLE satislar DROP COLUMN IF EXISTS tutar_doviz;
-ALTER TABLE satislar DROP COLUMN IF EXISTS kdv_toplami_doviz;
-ALTER TABLE satislar DROP COLUMN IF EXISTS indirim_toplami_doviz;
-ALTER TABLE satislar DROP COLUMN IF EXISTS genel_tutar_doviz;
-DROP TABLE IF EXISTS doviz_kurlari;
